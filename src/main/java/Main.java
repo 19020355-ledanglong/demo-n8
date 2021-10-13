14db171c050b157245e0a0c11e9a76ff5c06f2dd
unknown <lelong1310119@gmail.com>
2021-10-13 23:08:43 +0700

    Message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,6 @@
 import java.lang.Math;
-public class Main extends Test {
-    public Main() {
-    }
-
-    public Main(int b) {
-        a = b;
-    }
-
-    public void setA(int a){
-        this.a = a;
-    }
-
+public class Main  {
     public static void main(String[] args) {
-        Main x = new Main(2);
         System.out.println(2 * (23.6 + 1.0));
     }
 }
