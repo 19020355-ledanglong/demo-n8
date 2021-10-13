43e8c57ece051ffd7a7979d205c1d9b805a2e037
19020355-ledanglong <91559403+19020355-ledanglong@users.noreply.github.com>
2021-10-13 15:03:54 +0700

    124

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
-public class Main {
-    /**
-     * this is main function.
-     */
+import java.lang.Math;
+public class Main extends Test {
+    public Main() {
+    }
+
+    public Main(int b) {
+        a = b;
+    }
+
+    public void setA(int a){
+        this.a = a;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello le dang long");
+        Main x = new Main(2);
+        System.out.println(2 * (23.6 + 1.0));
     }
 }
